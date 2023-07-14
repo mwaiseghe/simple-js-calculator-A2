91ac812a7dd0859f06d2b054eb7f5d4feeeec2b8
mwaiseghe <mwaiseghe23@gmail.com>
2023-07-14 01:00:55 -0700

    first commit

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 let firstNumber = document.getElementById('firstNumber');
 let secondNumber = document.getElementById('lastNumber');
 let operator = document.getElementById('operator');
+let clearButton = document.querySelector('.clearButton')
 
 let calcForm = document.querySelector(".calcForm");
 
+function getResults(){
+    let results = eval(firstNumber.value + operator.value + secondNumber.value)
+    return results
+}
+
 calcForm.addEventListener('submit', function(e){
     e.preventDefault();
-    let results = eval(firstNumber.value + operator.value + secondNumber.value)
-    console.log(results); 
+    
+    let results = getResults()
 
     let answer = document.querySelector('.answer')
     answer.innerHTML = `
          <h2>${results}</h2>
     `
+    addClearbutton()
 })
+
+function addClearbutton(){
+    clearButton.innerHTML = `
+        <button>Clear</button>
+    `
+}
+
+
+clearButton.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calcForm.reset();
+})
+
 
